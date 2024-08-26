d223156494ec23e73330efcf8b4ce4ec7a9529e0
sraverkar.encora <sachin.raverkar@encora.com>
2024-08-26 23:50:58 +0530

    Formatted code

--- src/test/java/com/encora/taskmanagement/TaskManagementServiceApplicationTests.java
+++ src/test/java/com/encora/taskmanagement/TaskManagementServiceApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles(value = {"test"})
-//@ImportAutoConfiguration(
+// @ImportAutoConfiguration(
 //    exclude = {
 //      DataSourceAutoConfiguration.class,
 //      MongoAutoConfiguration.class,
